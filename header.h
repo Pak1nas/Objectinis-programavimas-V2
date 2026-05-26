56d2053da5d31d6f0fbc5c2a02aa3632840891e0
GustasNa <gnavierauskas@gmail.com>
2026-05-27 00:05:58 +0300

    Create header.h

---
+++ header.h
@@ -0,0 +1,46 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include "zmogus.h"
+#include <string>
+#include <vector>
+
+class Student : public zmogus {
+private:
+    std::vector<double> nd_;
+    double egz_;
+    double vid_;
+    double med_;
+
+public:
+    Student();
+    Student(const std::string& v, const std::string& p, const std::vector<double>& nd, double egz);
+
+    //Rule of five
+    Student(const Student& other);
+    Student(Student&& other) noexcept;
+
+    Student& operator=(const Student& other);
+    Student& operator=(Student&& other) noexcept;
+
+    ~Student();
+
+    void skaiciuoti();
+
+    double vid() const;
+    double med() const;
+
+    void printInfo() const override;
+
+    friend std::ostream& operator<<(std::ostream& os, const Student& s);
+    friend std::istream& operator>>(std::istream& is, Student& s);
+};
+
+int s_int();
+double s_double();
+int atsitiktinis();
+
+void spausdinti_lentele(const std::vector<Student>& A, int pas);
+void generuoti_studentus(int kiekismok, int kiekpaz);
+
+#endif
