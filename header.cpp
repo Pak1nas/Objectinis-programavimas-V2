f8ba6e22484d33aa964cc1d84351f2716855b491
GustasNa <gnavierauskas@gmail.com>
2026-05-29 10:52:19 +0300

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -63,37 +63,70 @@ void skaiciuoti ( stud &s )
     else
         s.med = ( kopija[d / 2 - 1] + kopija[d / 2] ) / 2.0;
 }
+void skaidymas(deque<stud>& A, deque<stud>& varg, deque<stud>& kiet, int pass){
 
-void spausdinti_lentele ( const deque<stud>& A, int pas )
+    if(pass==1){
+            for(const auto &s : A){
+                if (s.vid>=5){
+                    kiet.push_back(s);
+                }
+                else
+                    varg.push_back(s);
+            }
+
+    }
+    if(pass==2){
+        auto it=A.begin();
+        while(it != A.end()){
+            if(it->vid < 5){
+            varg.push_back(*it);
+               it=A.erase(it);
+            }
+            else{
+                ++it;
+            }
+        }
+    }
+    if(pass==3){
+        auto it=std::partition(A.begin(), A.end(), [](const stud& s){return s.vid<5;});
+
+        varg.insert(varg.end(), A.begin(), it);
+        kiet.insert(kiet.end(), it, A.end());
+    }
+}
+
+
+void spausdinti_lentele ( const deque<stud>& A, int pas , int pass)
 {
-    ofstream kiet ( "kietekai.txt" );
-    ofstream varg ( "vargsiukai.txt" );
+    ofstream kieti ( "kietekai.txt" );
+    ofstream vargi ( "vargsiukai.txt" );
 
-    if ( pas == 1 || pas == 3 )
-    {
-        cout << endl << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl;
-        cout << string ( 70, '-' ) << endl;
-    }
-
-    if ( pas == 2 || pas == 3 )
-    {
-        kiet << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl << string ( 70, '-' ) << endl;
-
-        varg << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl << string ( 70, '-' ) << endl;
-    }
-
-    for ( const auto &s : A )
-    {
         if ( pas == 1 || pas == 3 )
         {
-            cout << left << setw ( 15 ) << s.pavarde << setw ( 15 ) << s.vardas << right << setw ( 20 ) << fixed << setprecision ( 2 ) << s.vid << setw ( 20 ) << s.med << endl;
+            cout << endl << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl;
+            cout << string ( 70, '-' ) << endl;
         }
 
-        if ( s.vid >= 5 )
-            kiet << s.pavarde << " " << s.vardas << " " << s.vid << " " << s.med << endl;
-        else
-            varg << s.pavarde << " " << s.vardas << " " << s.vid << " " << s.med << endl;
-    }
+        if ( pas == 2 || pas == 3 )
+        {
+            kieti << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl << string ( 70, '-' ) << endl;
+
+            vargi << left << setw ( 15 ) << "Pavarde" << setw ( 15 ) << "Vardas" << right << setw ( 20 ) << "Galutinis (Vid.)" << setw ( 20 ) << "Galutinis (Med.)" << endl << string ( 70, '-' ) << endl;
+        }
+
+        for ( const auto &s : A )
+        {
+            if ( pas == 1 || pas == 3 )
+            {
+                cout << left << setw ( 15 ) << s.pavarde << setw ( 15 ) << s.vardas << right << setw ( 20 ) << fixed << setprecision ( 2 ) << s.vid << setw ( 20 ) << s.med << endl;
+            }
+
+            if ( s.vid >= 5 )
+                kieti << s.pavarde << " " << s.vardas << " " << s.vid << " " << s.med << endl;
+            else
+                vargi << s.pavarde << " " << s.vardas << " " << s.vid << " " << s.med << endl;
+        }
+
 }
 
 void generuoti_studentus ( int kiekismok, int kiekpaz )
@@ -117,3 +150,4 @@ void generuoti_studentus ( int kiekismok, int kiekpaz )
         is << " " << atsitiktinis() << endl;
     }
 }
+
